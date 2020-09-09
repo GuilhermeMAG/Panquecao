4b7edc8e622cea93643c10fdfc5cdac57a99267d
Guilherme Augusto Borges Casagrande <38353118+GuilhermeMAG@users.noreply.github.com>
2020-09-09 16:23:20 -0300

    Initial commit

---
+++ Panquecao/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const PanquecaApp = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Panquecão Express</Text>
+    </View>
+  );
+};
+export default PanquecaApp;
